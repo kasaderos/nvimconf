95c0566963597df856b0caf8d129237b31804b58
Berdibek Kaldarov <b.kaldarov@group-ib.com>
2026-04-03 09:33:35 +0500

    add gitsigns plugin

--- lua/nomad/plugins/nvim_tree.lua
+++ lua/nomad/plugins/nvim_tree.lua
@@ -1,27 +1,34 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,   dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup({
-      -- config
-      sort = {
-        sorter = "case_sensitive",
-      },
-      view = {
-        width = 30,
-      },
-      renderer = {
-        group_empty = true,
-      },
-      filters = {
-        dotfiles = false,
-      },
-    })
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,   dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("nvim-tree").setup({
+            -- config
+            sort = {
+                sorter = "case_sensitive",
+            },
+            view = {
+                width = 30,
+            },
+            renderer = {
+                group_empty = true,
+                icons = {
+                    glyphs = {
+                        git = {
+                            untracked = "+",
+                        },
+                    },
+                },
+            },
+            filters = {
+                dotfiles = false,
+            },
+        })
 
-    -- toggle open/close
-    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle nvim-tree" })
-  end,
+        -- toggle open/close
+        vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle nvim-tree" })
+    end,
 }
